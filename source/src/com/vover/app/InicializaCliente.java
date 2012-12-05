6430721c6dda99f1ef2460daf5bebf6c9a399514
Charles Tim Batista Garrocho <ctgarrocho@gmail.com>
2012-12-05 10:54:58 -0200

    Alterado: Estrutura do Metodo Main.

--- source/src/com/vover/app/InicializaCliente.java
+++ source/src/com/vover/app/InicializaCliente.java
@@ -1,6 +1,9 @@
 package com.vover.app;
 
+import java.text.ParseException;
+
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import com.vover.gui.anonimo.JanelaAnonimo;
 
@@ -8,15 +11,9 @@ import de.javasoft.plaf.synthetica.SyntheticaBlackEyeLookAndFeel;
 
 public class InicializaCliente {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedLookAndFeelException, ParseException {
 
-		UIManager.put("Synthetica.window.decoration", Boolean.FALSE);
-		try {
-			UIManager.setLookAndFeel(new SyntheticaBlackEyeLookAndFeel());
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+		UIManager.setLookAndFeel(new SyntheticaBlackEyeLookAndFeel());
 		new JanelaAnonimo();
 	}
 }
